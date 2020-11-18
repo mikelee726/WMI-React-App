373c7913d58042588c2f410b530963560bf98644
Paneer Shekar <paneer@pearlsolutions.com>
2020-11-17 22:48:14 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./App.css";
+import data from "./honda_wmi.json";
+
+function App() {
+  const keys = ["Name", "WMI", "Country", "CreatedOn", "VehicleType"];
+
+  const getRowsJsx = () => {
+    return data.map((d) => {
+      const wmi = d.WMI;
+      return (
+        <tr key={wmi}>
+          {keys.map((k) => (
+            <td key={`${wmi}-${k}`}>{d[k]}</td>
+          ))}
+        </tr>
+      );
+    });
+  };
+
+  return (
+    <div className="App">
+      <header>WMI Data - Honda</header>
+      <table>
+        <thead>
+          <tr>
+            `
+            {keys.map((k) => (
+              <th key={k}>{k}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>{getRowsJsx()}</tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
